c63182793015facc2570ce290ece95de64be489c
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2019-11-01 22:19:30 +0100

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,2 +1,7 @@
 As we have mentioned previously, it is legal to make more than one assignment to the same variable. A new assignment
 makes an existing variable refer to a new value (and stop referring to the old value).
+
+    airtime_remaining = 15
+    print(airtime_remaining)
+    airtime_remaining = 7
+    print(airtime_remaining)
