87c114fce8a8b8642249c882f433583cc9c3744e
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2019-11-20 20:04:42 +0100

    Update An_Example.py

--- An_Example.py
+++ An_Example.py
@@ -17,3 +17,13 @@ The following program implements a simple guessing game:
     else:
       break
   input("\n\nGreat, you got it in "+str(guesses)+" guesses!\n\n")
+
+This program makes use of the mathematical law of trichotomy (given real numbers a and b, exactly one of these
+three must be true: a > b, a < b, or a == b).
+At line 18 there is a call to the input function, but we don’t do anything with the result, not even assign it to a variable.
+This is legal in Python. Here it has the effect of popping up the input dialog window and waiting for the user to respond
+before the program terminates. Programmers often use the trick of doing some extra input at the end of a script, just
+to keep the window open.
+Also notice the use of the message variable, initially an empty string, on lines 6, 12 and 14. Each time through the
+loop we extend the message being displayed: this allows us to display the program’s feedback right at the same place
+as we’re asking for the next guess.
